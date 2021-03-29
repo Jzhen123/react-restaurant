0a921ba0346ff01ac8e2cd623e8de23ea72654dc
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-29 14:03:48 -0400

    Going into dev branch

--- src/App.js
+++ src/App.js
@@ -5,33 +5,40 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {data: []};
+    this.state = {
+      sections: {
+         breakfast: { food : "eggs"},
+         lunch: {},
+         appetizers: {},
+         dinner: {},
+         sides: {},
+         desserts: {},
+         drinks: {},
+      }
+    };
   }
   
   componentDidMount() {
-    axios.get('http://awesomeincbootcampapi-ianrios529550.codeanyapp.com:3000/public/api/menu/type/5')
-    .then(
-      (response) => {
-        this.setState({
-          isLoaded: true,
-          data: response.data
-        })
-        console.log(this.state.data)
-      }
-    ) 
+    // axios.get('http://awesomeincbootcampapi-ianrios529550.codeanyapp.com:3000/public/api/menu/type/5')
+    // .then((response) => {this.setState({ data: response.data })}) 
+    console.log(Object.keys(this.state.sections))
+
+    // for (let i = 0)
+
   }
+
   
- 
   
   render() {
     return (
-      <ul>
-          {this.state.data.map(item => (
-            <li key={item.id}>
-              {item.name} {item.id}
-            </li>
-          ))}
-        </ul>
+      "Hi"
+      // <ul>
+      //     {this.state.data.map(item => (
+      //       <li key={item.id}>
+      //         {item.name}
+      //       </li>
+      //     ))}
+      //   </ul>
     )
   }
 }

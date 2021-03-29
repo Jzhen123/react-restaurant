d8aa5c1e9b54ddba1cd146dd46fca0a19d73f9d4
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-29 12:22:41 -0400

    Added API call to an object that we can use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import axios from 'axios';
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+  
+  componentDidMount() {
+    axios.get('http://awesomeincbootcampapi-ianrios529550.codeanyapp.com:3000/public/api/menu/type/5')
+    .then(
+      (response) => {
+        this.setState({
+          isLoaded: true,
+          data: response.data
+        })
+        console.log(this.state.data)
+      }
+    ) 
+  }
+  
+ 
+  
   render() {
     return (
-      
+      <ul>
+          {this.state.data.map(item => (
+            <li key={item.id}>
+              {item.name} {item.id}
+            </li>
+          ))}
+        </ul>
     )
   }
 }
-
 export default App;

2ad34ec455b759b3d56babbd6f292d8d77c0008c
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-31 16:02:46 -0400

    Added Some styling to the Menu

--- src/App.js
+++ src/App.js
@@ -33,20 +33,21 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <div className="card m-5 p-3 text-center border border- border-5">
-          <h1 className="p-3">Restaurant Name</h1>
-          <h3>Address</h3>
-          <h3>Hours</h3>
-          <div className="accordion" id="accordionExample">
-            {
-              this.menu.map((item, index) => {
-                return (
-                  <MenuSection key={index} menuSection={item} />
-                )
-              })
-            }
+        <div id="Background" className="card m-5 p-3 text-center border border-dark">
+            <h1 className="pt-3">Le Ian's</h1>
+            <h3>Avenue de Awesome Inc, Paris, France</h3>
+            <h3>Mon-Fri: 09:30 - 20:30 &nbsp;&nbsp;&nbsp; Sat-Sun: 11:30 - 20:30</h3>
+            <div className="accordion" id="accordionExample">
+              {
+                this.menu.map((item, index) => {
+                  return (
+                    <MenuSection key={index} menuSection={item} />
+                  )
+                })
+              }
+              <h3>Menu prices based on if we need more money</h3>
+            </div>
           </div>
-        </div>
       </>
     )
   }
